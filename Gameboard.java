5e58899316314bab1b78df327304f8dfd4c96669
tinaia <tinaia@ulrik.uio.no>
2025-01-26 01:50:42 +0100

    Adding changes fpr the enum Color and the classes Player, Place and Gameboard (not finished).

--- Gameboard.java
+++ Gameboard.java
@@ -1,4 +1,43 @@
 //arraylist<object> for brettet
 //arraylist for spillere
+import java.util.ArrayList;
+import java.lang.Math;
 
-//metoder for
+public class Gameboard {
+    ArrayList<Object> gameBoard = new ArrayList<>();
+    ArrayList<String> gamePieces = new ArrayList<>();
+    ArrayList<Player> players = new ArrayList<>();
+    String[] chance = {};
+    String[] communityChest = {};
+
+    public Gameboard() {
+        fillPieceArray();
+    }
+
+    public void fillPieceArray() {
+        gamePieces.add("Dog");
+        gamePieces.add("Hat");
+        gamePieces.add("Iron");
+        gamePieces.add("Car");
+        gamePieces.add("Boot");
+        gamePieces.add("Ship");
+        gamePieces.add("Wheelbarrow");
+        gamePieces.add("Thimble");
+    }
+
+    public void pickGamePiece(Player player) {
+        int chosenInt = (int)(Math.random() * gamePieces.size());
+        String chosenString = gamePieces.get(chosenInt);
+        player.setGamePiece(chosenString);
+        System.out.println("Your game piece is the " + chosenString);
+        gamePieces.remove(chosenString);
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+    }
+}
